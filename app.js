dcd52a4b982040e44b64ee9d0d3880a4099d7934
dwanerich <dwanerich@gmail.com>
2021-11-02 00:29:13 -0400

    added strike thru on click / delete on dblClick

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 // Variables
 let submitButton = document.getElementById('submit')
 let foodInput = document.getElementById('cuisineInput')
-let cuisineInput = document.getElementById('cuisineContainer')
+let cuisineList = document.getElementById('cuisineContainer')
 
 // Event Listeners
 submitButton.addEventListener('click', function() {
-    let cuisineDiv= document.createElement('div')
+    let cuisineDiv = document.createElement('div')
+    cuisineDiv.classList.add('div-style')
     cuisineDiv.innerText = foodInput.value;
-    cuisineContainer.appendChild(cuisineDiv);
+    cuisineList.appendChild(cuisineDiv);
     foodInput.value = '';
+    cuisineDiv.addEventListener('click', function() {
+        cuisineDiv.style.textDecoration = 'line-through'
+    })
+    
+    cuisineList.addEventListener('dblclick', function(){
+        cuisineList.removeChild(cuisineDiv);
+    })
 })
