d7be78db9004fda8bbb888f38208ddc7d450937f
dwanerich <dwanerich@gmail.com>
2021-11-01 20:01:09 -0400

    added boostrap theme, a form & jS variables

---
+++ app.js
@@ -0,0 +1,9 @@
+// Selector
+
+const cuisineInput = document.getElementById('cuisine-input');
+const submitButton = document.getElementById('input-button')
+const cuisineList = document.getElementById('cuisine-list')
+
+// Event Listeners
+
+//  Functions
