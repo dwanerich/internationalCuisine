b3170fb3c4706775d635d4a053db90f0ce6c81ef
dwanerich <dwanerich@gmail.com>
2021-11-07 21:42:52 -0500

    added empty form input alert with 2 sec timeout

--- app.js
+++ app.js
@@ -3,23 +3,34 @@
 // Variables
 let submitButton = document.getElementById('submit')
 let foodInput = document.getElementById('cuisineInput')
-let ingredientInput = document.getElementById('ingredientInput')
 let cuisineList = document.getElementById('cuisineContainer')
+let ingredientInput = document.getElementById('ingredientInput')
+let inputAlert = document.getElementById('input-alert')
 
 // Event Listeners
 submitButton.addEventListener('click', function(e) {
     e.preventDefault();
+
+if (foodInput.value === '' || cuisineInput.value === '') {
+    inputAlert.innerHTML = "Please enter correct values";
+
+    setTimeout(() => inputAlert.remove(), 3000)
+
+
+} else {
     let cuisineDiv = document.createElement('div')
     cuisineDiv.classList.add('divStyle')
-    cuisineDiv.innerHTML = ` ${foodInput.value} ${ingredientInput.value}`
+    cuisineDiv.innerHTML = `${foodInput.value} ${ingredientInput.value}`
     cuisineList.appendChild(cuisineDiv);
     foodInput.value = ''; ingredientInput.value = '';
     cuisineDiv.addEventListener('click', function() {
         cuisineDiv.style.textDecoration = 'line-through'
-    })
+    })}
     
     cuisineDiv.addEventListener('dblclick', function(){
         cuisineList.removeChild(cuisineDiv);
     })
 })
+
+// g paw many more much smaller steps
 
